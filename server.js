e9e2388eb56cb1a7157c52c3ac71690880ac3f3b
VishalMreddy <vishalreddym37@gmail.com>
2024-03-20 14:54:54 +0530

    Added the mongodb URI

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
+const mongoose = require('mongoose')
+require("dotenv").config() 
 const port=3000;
 
 app.get('/ping',(req,res)=>{
@@ -10,3 +12,14 @@ app.listen(port,()=>{
     console.log(`App running on port: ${port}`)
 
 });
+app.get('/home', (req,res)=> {
+    mongoose.connect(process.env.URI)
+    .then(()=>{
+        console.log("connected to mongoDB")
+    })
+    .catch((err)=>{
+        console.error(err) 
+    })
+    const dbstatus = mongoose.connection ? "connected":"Disconnected"
+    res.send(`Dastabase connection status :${dbstatus} `)
+})
